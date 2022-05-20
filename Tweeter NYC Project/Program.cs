b53d4041282110bb045e5961fc65597c5bc5ab06
ALEXANDROS KYRIAZIS <alexandroskyriazis5@gmail.com>
2022-05-20 18:50:19 +0300

    Added connection credentials and connection

--- Tweeter NYC Project/Program.cs
+++ Tweeter NYC Project/Program.cs
@@ -9,5 +9,11 @@ namespace TeeterNYCProject
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            //connect to twitterAPI
+            var userCredentials = new TwitterCredentials("4ygYBawbzLCnoE9lsr0xtn5Qs", "MlKShTX7Dtcg7otZSxY5BcZO9e4DVgyYurd4SDcA8WIFVZ15Sh", "1468216303420903425-blYdPOgof5hmgrQUfrHKiHgBn8cy2g", "wocRHayEr7CV5x1sPAlVjS3Q7KUu0S2attQ4oAlO1290v");
+            var userClient = new TwitterClient(userCredentials);
+        }
     }
 }
