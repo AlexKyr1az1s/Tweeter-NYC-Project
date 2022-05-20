0fb7fc08149ce2d61b64d25ccc02a6d7649877c1
ALEXANDROS KYRIAZIS <alexandroskyriazis5@gmail.com>
2022-05-20 19:00:42 +0300

    Added a filter stream for collecting tweets for the keyword javascript

--- Tweeter NYC Project/Program.cs
+++ Tweeter NYC Project/Program.cs
@@ -9,11 +9,22 @@ namespace TeeterNYCProject
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             //connect to twitterAPI
             var userCredentials = new TwitterCredentials("4ygYBawbzLCnoE9lsr0xtn5Qs", "MlKShTX7Dtcg7otZSxY5BcZO9e4DVgyYurd4SDcA8WIFVZ15Sh", "1468216303420903425-blYdPOgof5hmgrQUfrHKiHgBn8cy2g", "wocRHayEr7CV5x1sPAlVjS3Q7KUu0S2attQ4oAlO1290v");
             var userClient = new TwitterClient(userCredentials);
+
+            //keywords for twt collection
+            var stream = userClient.Streams.CreateFilteredStream();
+            stream.AddTrack("Javascript");
+
+            stream.MatchingTweetReceived += (sender, eventReceived) =>
+            {
+                Console.WriteLine(eventReceived.Tweet);
+            };
+
+            await stream.StartMatchingAnyConditionAsync();
         }
     }
 }
