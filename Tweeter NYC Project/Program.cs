be799df3a77311a5af4dfd9f6bb74e997c590741
ALEXANDROS KYRIAZIS <alexandroskyriazis5@gmail.com>
2022-05-20 18:32:01 +0300

    Downloaded Tweetinvi and added usings (Tweetinvi and tweetinvi.Models)

--- Tweeter NYC Project/Program.cs
+++ Tweeter NYC Project/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Models;
+
+
+
+namespace TeeterNYCProject
+{
+    class Program
+    {
+    }
+}
